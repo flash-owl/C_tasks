36a7f16eb47a9b70bdbad36f07c12401d5a018a5
flash-owl <40030849+flash-owl@users.noreply.github.com>
2020-04-05 22:10:41 +0300

    Add files via upload

---
+++ Task1.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+  srand(time(NULL));
+  int N = 10;
+  int a[N];
+  int i, max, min, pos1, pos2;
+
+  a[0] = rand()%50; 
+  max = a[0];
+  min = a[0];
+  printf("%d\n", a[0]);
+  for(i = 1; i < N; i++){
+    a[i] = rand()%50;
+
+    if(a[i] > max){
+      max = a[i];
+      pos1 = i;
+    }
+    if(a[i] < min){
+      min = a[i];
+      pos2 = i;
+    }
+    printf("%d\n", a[i]);
+
+  }
+  a[pos1] = min;
+  a[pos2] = max;
+
+  printf("\n");
+
+  for(i = 0; i < N; i++){
+    printf("%d\n", a[i]);
+  }
+  return 0;
+}
